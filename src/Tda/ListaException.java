408c4fa466e916c1ad287ac2214cfb9eb7cf7db9
Arturo-hnavarro <hernandezn.arturo@gmail.com>
2017-05-16 11:18:45 -0600

    Agregamos la listaEnlazadaSimple

---
+++ src/Tda/ListaException.java
@@ -0,0 +1,12 @@
+/** 
+ * File     : ListaException.java
+ * @author  : Prof. Gilberth Chaves Avila
+ * Date     : 2017-04-25
+ */
+package Tda;
+public class ListaException extends Exception {
+    //Constructor
+    public ListaException(String err){
+        super(err);
+    }//Constructor
+}
